7e0a124cfbda8a2b1684fb97df1d56e917a915ed
Aniket Yadav <aniani4848@gmail.com>
2022-02-04 21:01:11 +0530

    Got problem in old client project, starting from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,23 @@
-import "./fonts/Ember-medium.ttf";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Signup from "./components/screens/Signup";
-import Signin from "./components/screens/Signin";
-import Home from "./components/screens/Home";
-import ProductDetail from "./components/screens/ProductDetail";
-import Header from "./components/layouts/Header";
-import CategoryNav from "./components/layouts/CategoryNav";
-import Footer from "./components/layouts/Footer";
-import Products from "./components/screens/Products";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <CategoryNav />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        {/* {This route is for development purpose only, delete it later} */}
-        <Route path="/search" element={<Products />} />
-        <Route path="/search/:keyword" element={<Products />} />
-      </Routes>
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
-import { positions, transitions, Provider as ALertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-
-const alert_options = {
-  timeout: 5000,
-  position: positions.BOTTOM_CENTER,
-  transition: transitions.SCALE,
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ALertProvider template={AlertTemplate} {...alert_options}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ALertProvider>
-  </Provider>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
